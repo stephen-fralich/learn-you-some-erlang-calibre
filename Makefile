61977eebe77ee9a4f8b8b5c5cd7acc95ce11063c
Stephen Fralich <stephen.fralich@gmail.com>
2023-01-01 20:59:06 -0800

    Add option to create pdf

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
-all:
-	php -d short_open_tag=off -d display_errors=1 main.php && \
+all: opf
 	ebook-convert build/book.opf learn-you-some-erlang.mobi
+opf:
+	php -d short_open_tag=off -d display_errors=1 main.php
+pdf: opf
+	ebook-convert build/book.opf learn-you-some-erlang.pdf
 clean:
-	rm -rf build learn-you-some-erlang.mobi
+	rm -rf build learn-you-some-erlang.*
--- config.php
+++ config.php
@@ -6,4 +6,4 @@ define('BUILD_DIR',      __DIR__ . '/build');
 define('EBOOK_TITLE',    'Learn You Some Erlang for Great Good!');
 define('EBOOK_AUTHOR',   'Frederic Trottier-Hebert');
 define('EBOOK_LANGUAGE', 'en-us');
-define('IMAGES',         'explain'); // Values: all all-titles all-alt-titles explain explain-titles explain-alt-titles none
+define('IMAGES',         'all'); // Values: all all-titles all-alt-titles explain explain-titles explain-alt-titles none
